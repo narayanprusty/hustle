3f339eacd5a164905826ad70ab199e24c318efde
saikat <saikatchakrabortty2@gmail.com>
2019-01-07 21:14:05 +0530

    Login page notifications added

--- imports/ui/pages/login/Login.jsx
+++ imports/ui/pages/login/Login.jsx
@@ -4,8 +4,7 @@ import { Meteor } from "meteor/meteor";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import {notify} from 'react-notify-toast';
-
+import { notify } from "react-notify-toast";
 
 import "./Login_client.scss";
 export default class Login extends Component {
@@ -40,21 +39,23 @@ export default class Login extends Component {
       },
       () => {
         Meteor.loginWithPassword(
-         {email: this.state.phone},
+          { email: this.state.phone },
           this.state.password,
           error => {
             if (error && error.reason) {
+              notify.show(error.reason, "error");
               this.setState({
                 login_formloading: false,
                 formSubmitError: error.reason
               });
             } else if (error && !error.reason) {
+              notify.show("Some Error Occured!", "error");
               this.setState({
                 login_formloading: false,
                 formSubmitError: "An error occured.please try again."
               });
             } else {
-              notify.show('Login successful!','success');
+              notify.show("Login successful!", "success");
               this.setState({
                 login_formloading: false,
                 formSubmitError: ""
@@ -94,6 +95,7 @@ export default class Login extends Component {
             className="button button-small button-energized"
             style={{ marginTop: "2em" }}
             onClick={this.loginHandler.bind(this)}
+            disabled={this.state.phone && this.state.password ? false : true}
           >
             Login
           </button>
