975cf052bb24f7a6cd4f914c248ac41dbd8314c0
saikat <saikatchakrabortty2@gmail.com>
2019-01-10 23:25:55 +0530

    added api key

--- imports/modules/config/client/index.js
+++ imports/modules/config/client/index.js
@@ -2,7 +2,7 @@
 module.exports={
     farePerMeter:0.04,
     fareUnit:'USD',
-    GAPIKEY: "AIzaSyBoL7QKlM1mECaAX7OglR1yEqiTBtoHoTo",
+    GAPIKEY: "AIzaSyAs2LAkFCJr8p4EzgIVtk169Qreykfi74Q",
     PUBNUB:{
         pubKey:'pub-c-f4bd0466-069e-4bd2-94cf-b299dc02f92a',
         subKey:'sub-c-b6590186-f6e1-11e8-b35b-72ed2feff2dd'
