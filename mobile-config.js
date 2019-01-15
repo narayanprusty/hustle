b7679fe2102f26e00a39ff8a69ded8f6f3bd305f
saikat <saikatchakrabortty2@gmail.com>
2019-01-15 17:39:51 +0530

    update mobile_config

--- mobile-config.js
+++ mobile-config.js
@@ -1,2 +1,10 @@
+App.info({
+    id: 'com.hustle.block.io',
+    name: 'Hustle',
+    version: "0.0.1"
+  });
+
 App.accessRule('*')
 App.setPreference('AutoHideSplashScreen' ,'true');
+App.setPreference('Orientation', 'default');
+App.setPreference('Orientation', 'all', 'ios');
