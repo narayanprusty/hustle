a8c0c2efff8eff7a88583477a60a2ff5eaf3ea63
saikat <saikatchakrabortty2@gmail.com>
2019-01-28 11:58:33 +0530

    methods updated

--- imports/startup/server/methods.js
+++ imports/startup/server/methods.js
@@ -1,6 +1,7 @@
 //require things
-require('../../api/emails/method');
-require('../../api/bookings/booking_methods');
-require('../../api/subscriptions/methods');
+require("../../api/emails/method");
+require("../../api/bookings/booking_methods");
+require("../../api/driverMeta/driverMeta_methods");
+require("../../api/subscriptions/methods");
 
 export {};
