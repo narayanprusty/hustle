66883d1806d86baef0c9fd0464a233a2ba84e1de
saikat <saikatchakrabortty2@gmail.com>
2019-01-28 11:58:27 +0530

    indexnt*

--- imports/modules/helpers/server/index.js
+++ imports/modules/helpers/server/index.js
@@ -1,44 +1,45 @@
 import crypto from "crypto";
 import ejs from "ejs";
-import Config from '../../config/server';
+import Config from "../../config/server";
 
-const EmailVerificationTemplate = require('../../template/email-verification');
+const EmailVerificationTemplate = require("../../template/email-verification");
 
 const EJSMapping = {
-  'email-verification.ejs': EmailVerificationTemplate,
+    "email-verification.ejs": EmailVerificationTemplate
 };
 
 function generateRandomString(placeholder, salt = "hustleThing") {
-  return `${new Date().getTime()}${crypto
-    .createHash("sha256")
-    .update(`${placeholder}${salt}${new Date().getTime()}`, "utf8")
-    .digest("hex")}`;
+    return `${new Date().getTime()}${crypto
+        .createHash("sha256")
+        .update(`${placeholder}${salt}${new Date().getTime()}`, "utf8")
+        .digest("hex")}`;
 }
 
-function generateURL(route){
-  return `${Config.apiHost.replace(":3000/", ':3000')}${route}`;
+function generateURL(route) {
+    return `${Config.apiHost.replace(":3000/", ":3000")}${route}`;
 }
-
 
 function generateCompleteURLForEmailVerification(query) {
-  return generateURL(`/email-verify?key=${query}`);
+    return generateURL(`/email-verify?key=${query}`);
 }
 
 function generateCompleteURLForPasswordReset(query) {
-  return generateURL(`/reset-password?key=${query}`);
+    return generateURL(`/reset-password?key=${query}`);
 }
 
-function getEJSTemplate({fileName}) {
-  return new Promise(resolve => {
-    if(!fileName){
-      fileName = "email-verification.ejs";
-    }
-    const content = EJSMapping[fileName];
-    resolve(ejs.compile(content, {
-      cache: true,
-      filename: fileName
-    }));
-  });
+function getEJSTemplate({ fileName }) {
+    return new Promise(resolve => {
+        if (!fileName) {
+            fileName = "email-verification.ejs";
+        }
+        const content = EJSMapping[fileName];
+        resolve(
+            ejs.compile(content, {
+                cache: true,
+                filename: fileName
+            })
+        );
+    });
 }
 
 export {
@@ -46,5 +47,5 @@ export {
     generateCompleteURLForEmailVerification,
     generateCompleteURLForPasswordReset,
     generateCompleteURLForUserInvite,
-    getEJSTemplate,
-  }
+    getEJSTemplate
+};
