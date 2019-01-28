b87ab27742de78cb4f8eb49f5a93162d9e477e45
saikat <saikatchakrabortty2@gmail.com>
2019-01-28 11:58:37 +0530

    in

--- server/main.js
+++ server/main.js
@@ -1,46 +1,46 @@
-import { Meteor } from 'meteor/meteor';
-require('../imports/startup/server');
+import { Meteor } from "meteor/meteor";
+require("../imports/startup/server");
 
-import Verifier from '../imports/api/emails/email-validator';
+import Verifier from "../imports/api/emails/email-validator";
 
 Accounts.validateLoginAttempt(function(options) {
-  if (!options.allowed) {
-    return false;
-  }
+    if (!options.allowed) {
+        return false;
+    }
 
-  // if (options.methodName == 'createUser') {
-  //   throw new Meteor.Error('unverified-account-created');
-  // }
-  //If user is driver, check if its marked verified by admin
-  // if(options.user.userType == 'Driver' && user.profile.driver_verified){
-  //   throw new Meteor.Error('Account not yet activated');
-  // }
-return true;
-  // if (options.user.emails[0].verified === true) {
-  //   return true;
-  // } else {
-  //   throw new Meteor.Error('email-not-verified', 'Your email is not verified. Kindly check your mail.');
-  // }
+    // if (options.methodName == 'createUser') {
+    //   throw new Meteor.Error('unverified-account-created');
+    // }
+    //If user is driver, check if its marked verified by admin
+    // if(options.user.userType == 'Driver' && user.profile.driver_verified){
+    //   throw new Meteor.Error('Account not yet activated');
+    // }
+    return true;
+    // if (options.user.emails[0].verified === true) {
+    //   return true;
+    // } else {
+    //   throw new Meteor.Error('email-not-verified', 'Your email is not verified. Kindly check your mail.');
+    // }
 });
 
 Accounts.onCreateUser(function(options, user) {
-  user.firstLogin = false;
-  user.profile = options.profile || {};
+    user.firstLogin = false;
+    user.profile = options.profile || {};
 
-  // Assigns first and last names to the newly created user object
-  user.profile.firstName = options.profile.firstName;
-  user.profile.lastName = options.profile.lastName;
-  // if(options.profile.userType == 'Driver'){
-  //   user.profile.driver_verified = false
-  // }
+    // Assigns first and last names to the newly created user object
+    user.profile.firstName = options.profile.firstName;
+    user.profile.lastName = options.profile.lastName;
+    // if(options.profile.userType == 'Driver'){
+    //   user.profile.driver_verified = false
+    // }
 
-  // if (!(!options.profile.firstName || options.profile.firstName === 'null' || options.profile.firstName === 'undefined')) {
-  //   Verifier.sendEmailVerification(user);
-  // }
+    // if (!(!options.profile.firstName || options.profile.firstName === 'null' || options.profile.firstName === 'undefined')) {
+    //   Verifier.sendEmailVerification(user);
+    // }
 
-  return user;
+    return user;
 });
 
 Meteor.startup(() => {
- console.log('>>>> Server Started <<<<')
+    console.log(">>>> Server Started <<<<");
 });
