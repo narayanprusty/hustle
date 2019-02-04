6d834c79504a167c00d1026e21d26b501194584b
saikat <saikatchakrabortty2@gmail.com>
2019-02-04 14:25:30 +0530

    changed browserrouter > Router

--- imports/ui/containers/app/App.jsx
+++ imports/ui/containers/app/App.jsx
@@ -1,37 +1,66 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import { withTracker } from "meteor/react-meteor-data";
 import Main from "../main/Main";
 import { Meteor } from "meteor/meteor";
-import Notifications from 'react-notify-toast';
+import Notifications from "react-notify-toast";
 
 import Register from "../../pages/register/Register";
 import EmailVerification from "../../pages/EmailVerification/EmailVerification";
 import Login from "../../pages/login/Login";
 
 requireAuth = RouteComponent => {
-  return () => {
-    return Meteor.userId() ? <RouteComponent /> : <Redirect to="/login" />;
-  };
+    return () => {
+        return Meteor.userId() ? <RouteComponent /> : <Redirect to="/login" />;
+    };
 };
 
 requireNotLoggedIn = RouteComponent => {
-  return () => {
-    return Meteor.userId() ? <Redirect to={localStorage.getItem("driverMode") ? '/app/driver/newreqs': "/app/home"}/> : <RouteComponent />;
-  };
+    return () => {
+        return Meteor.userId() ? (
+            <Redirect
+                to={
+                    localStorage.getItem("driverMode")
+                        ? "/app/driver/newreqs"
+                        : "/app/home"
+                }
+            />
+        ) : (
+            <RouteComponent />
+        );
+    };
 };
 
-
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" render={() => <Redirect to="/login" />} />
-      <Route exact path="/login" render={this.requireNotLoggedIn(Login)} />
-      <Route exact path="/signup" render={this.requireNotLoggedIn(Register)} />
-      <Route exact path="/app" render={() => <Redirect to={localStorage.getItem("driverMode") ? '/app/driver/newreqs': "/app/home"} />} />
-      <Route exact path="/app/*" render={this.requireAuth(Main)} />
-    </Switch>
-  </BrowserRouter>
+    <Router>
+        <Switch>
+            <Route exact path="/" render={() => <Redirect to="/login" />} />
+            <Route
+                exact
+                path="/login"
+                render={this.requireNotLoggedIn(Login)}
+            />
+            <Route
+                exact
+                path="/signup"
+                render={this.requireNotLoggedIn(Register)}
+            />
+            <Route
+                exact
+                path="/app"
+                render={() => (
+                    <Redirect
+                        to={
+                            localStorage.getItem("driverMode")
+                                ? "/app/driver/newreqs"
+                                : "/app/home"
+                        }
+                    />
+                )}
+            />
+            <Route exact path="/app/*" render={this.requireAuth(Main)} />
+        </Switch>
+    </Router>
 );
 
 export default App;
