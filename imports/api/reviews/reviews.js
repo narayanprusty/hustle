c77c3be2e9a301d5c05eff2c7f410e5f5a1feacc
saikat <saikatchakrabortty2@gmail.com>
2019-02-05 16:43:55 +0530

    WIP: reviews function made

--- imports/api/reviews/reviews.js
+++ imports/api/reviews/reviews.js
@@ -1,4 +1,6 @@
+import shortid from "shortid";
 import Blockcluster from "blockcluster";
+import { updateReview, getDriver } from "../driverMeta/driverMeta";
 import config from "../../modules/config/server";
 
 const node = new Blockcluster.Dynamo({
@@ -6,14 +8,148 @@ const node = new Blockcluster.Dynamo({
     instanceId: config.BLOCKCLUSTER.instanceId
 });
 
-const rateDriver = async () => {};
+/**
+ *  User reviews Driver
+ * @param {String} driverId
+ * @param {String} message
+ * @param {Number} rateVal
+ */
+const rateDriver = async ({ driverId, message, rateVal }) => {
+    const userId = Meteor.userId();
+    const identifier = Date.now() + shortid.generate();
+    const data = {
+        driverId: driverId,
+        riderId: userId,
+        message: message,
+        rating: rateVal,
+        ratedBy: "rider"
+    };
+    await node.callAPI("assets/issueSoloAsset", {
+        assetName: config.ASSET.Reviews,
+        fromAccount: node.getWeb3().eth.accounts[0],
+        toAccount: node.getWeb3().eth.accounts[0],
+        identifier: identifier
+    });
+    const txId = await node.callAPI("assets/updateAssetInfo", {
+        assetName: config.ASSET.Reviews,
+        fromAccount: node.getWeb3().eth.accounts[0],
+        identifier: identifier,
+        public: data
+    });
+    await updateReview(driverId, rateVal);
+    return { txId: txId };
+};
 
-const rateRider = async () => {};
-const fetchAvgRatingDriver = async () => {};
+const updateReviewRider = async (riderId, rateVal) => {
+    const { profile } = Meteor.users.find(
+        { _id: riderId },
+        { fields: { profile: true } }
+    );
+    let currentRating;
+    let NewNoOfRating;
+    if (profile.noOfRating) {
+        NewNoOfRating = profile.noOfRating + 1;
+        currentRating =
+            (profile.noOfRating * profile.avgRating + rateVal) / NewNoOfRating;
+    } else {
+        NewNoOfRating = 1;
+        currentRating = rateVal;
+    }
 
-const fetchAvgRatingRider = async () => {};
-const fetchRatingsDriver = async () => {};
-const fetchRatingsRider = async () => {};
+    return Meteor.users.update(
+        { _id: riderId },
+        {
+            $set: {
+                "profile.avgRating": currentRating,
+                "profile.noOfRating": NewNoOfRating
+            }
+        }
+    );
+};
+
+/**
+ *  User reviews Rider
+ * @param {String} driverId
+ * @param {String} message
+ * @param {Number} rateVal
+ */
+
+const rateRider = async ({ riderId, message, rateVal }) => {
+    const userId = Meteor.userId();
+    const identifier = Date.now() + shortid.generate();
+    const data = {
+        driverId: userId,
+        riderId: riderId,
+        message: message,
+        rating: rateVal,
+        ratedBy: "driver"
+    };
+    await node.callAPI("assets/issueSoloAsset", {
+        assetName: config.ASSET.Reviews,
+        fromAccount: node.getWeb3().eth.accounts[0],
+        toAccount: node.getWeb3().eth.accounts[0],
+        identifier: identifier
+    });
+    const txId = await node.callAPI("assets/updateAssetInfo", {
+        assetName: config.ASSET.Reviews,
+        fromAccount: node.getWeb3().eth.accounts[0],
+        identifier: identifier,
+        public: data
+    });
+    await updateReviewRider(riderId, rateVal);
+
+    return { txId: txId };
+};
+const fetchAvgRatingDriver = async driverId => {
+    const driverData = getDriver(driverId);
+    return {
+        rating: driverData.avgRating ? driverData.avgRating : 5,
+        totalRating: driverData.noOfRating ? driverData.noOfRating : 0,
+        totalRides: driverData.totalNumberOfRide
+            ? driverData.totalNumberOfRide
+            : 0
+    };
+};
+
+const fetchAvgRatingRider = async riderId => {
+    const { profile } = Meteor.users.find(
+        { _id: riderId },
+        { fields: { profile: true } }
+    );
+    return {
+        rating: profile.avgRating ? profile.avgRating : 5,
+        totalRating: profile.noOfRating ? profile.noOfRating : 0,
+        totalRides: profile.totalNumberOfRide ? profile.totalNumberOfRide : 0
+    };
+};
+const fetchRatingsDriver = async ({ page, driverId }) => {
+    const data = await node.callAPI("assets/search", {
+        $query: {
+            assetName: config.ASSET.Reviews,
+            driverId: driverId
+        },
+        $limit: page * 10,
+        $skip: page * 10 - 10,
+        $sort: {
+            _id: -1
+        }
+    });
+    return data;
+};
+const fetchRatingsRider = async ({ page, userId }) => {
+    const data = await node.callAPI("assets/search", {
+        $query: {
+            assetName: config.ASSET.Reviews,
+            riderId: userId
+        },
+        $limit: page * 10,
+        $skip: page * 10 - 10,
+        $sort: {
+            _id: -1
+        }
+    });
+    return data;
+};
 export {
     rateDriver,
     rateRider,
