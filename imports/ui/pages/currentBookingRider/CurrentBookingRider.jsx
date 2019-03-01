85b6e1522d53870ed3e653da7b8a4267b8deb777
saikat <saikatchakrabortty2@gmail.com>
2019-03-01 15:06:52 +0530

    change html/css to svg

--- imports/ui/pages/currentBookingRider/CurrentBookingRider.jsx
+++ imports/ui/pages/currentBookingRider/CurrentBookingRider.jsx
@@ -20,14 +20,16 @@ const Marker = ({ metaData, deg }) => (
             <span className="pulse_current" style={{ zIndex: 2 }} />
         )}
         {metaData == "cartop" && (
-            <div
-                className="cartop cartop-red"
-                style={{ transform: "rotate(" + deg ? deg : 30 + ")" }}
+            <svg
+                width="100"
+                height="100"
+                id="Capa_1"
+                transform={"rotate(" + (deg ? deg : 30) + ")"}
             >
-                <div className="cartop-front" />
-                <div className="cartop-middle" />
-                <div className="cartop-back" />
-            </div>
+                <g xmlns="http://www.w3.org/2000/svg">
+                    <path d="M29.395,0H17.636c-3.117,0-5.643,3.467-5.643,6.584v34.804c0,3.116,2.526,5.644,5.643,5.644h11.759   c3.116,0,5.644-2.527,5.644-5.644V6.584C35.037,3.467,32.511,0,29.395,0z M34.05,14.188v11.665l-2.729,0.351v-4.806L34.05,14.188z    M32.618,10.773c-1.016,3.9-2.219,8.51-2.219,8.51H16.631l-2.222-8.51C14.41,10.773,23.293,7.755,32.618,10.773z M15.741,21.713   v4.492l-2.73-0.349V14.502L15.741,21.713z M13.011,37.938V27.579l2.73,0.343v8.196L13.011,37.938z M14.568,40.882l2.218-3.336   h13.771l2.219,3.336H14.568z M31.321,35.805v-7.872l2.729-0.355v10.048L31.321,35.805z" />
+                </g>
+            </svg>
         )}
         {metaData != "current" && metaData != "cartop" && (
             <div>
@@ -870,10 +872,18 @@ class CurrentBookingRider extends Component {
                                     this.toggleChatBox();
 
                                     Meteor.setTimeout(() => {
-                                        document.getElementsByClassName("rcw-close-button")[0].addEventListener("click", (e) => {
-                                            window.scrollTo(0, 0)
-                                        }, false)
-                                    }, 1000)
+                                        document
+                                            .getElementsByClassName(
+                                                "rcw-close-button"
+                                            )[0]
+                                            .addEventListener(
+                                                "click",
+                                                e => {
+                                                    window.scrollTo(0, 0);
+                                                },
+                                                false
+                                            );
+                                    }, 1000);
                                 }}
                             >
                                 <i
@@ -1045,7 +1055,7 @@ class CurrentBookingRider extends Component {
                             badge={this.state.badge}
                             autofocus={false}
                             onClick={() => {
-                                this.setState({ badge: 0 })
+                                this.setState({ badge: 0 });
                             }}
                             handleNewUserMessage={this.handleNewUserMessage}
                             launcher={handleToggle =>
