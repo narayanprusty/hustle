ff3a0e2939ffb337a7a12f5e3ca6004d7b020fd5
saikat <saikatchakrabortty2@gmail.com>
2019-03-07 18:14:36 +0530

    Avatar uploading functions and rules applied

--- imports/ui/pages/Profile/Profile.jsx
+++ imports/ui/pages/Profile/Profile.jsx
@@ -3,14 +3,22 @@ import { Link, withRouter } from "react-router-dom";
 import { notify } from "react-notify-toast";
 import Ratings from "react-rating";
 import LaddaButton, { L, SLIDE_UP } from "react-ladda";
-import "./Profile_client.scss";
 import { Meteor } from "meteor/meteor";
+import "./Profile_client.scss";
+import { S } from "react-ladda/dist/constants";
+
 class Profile extends Component {
     constructor(props) {
         super(props);
-        this.state = { value: "" };
+        this.state = { value: "", avgUserRating: 0, avgRating: 0 };
         this.isDriver = false;
     }
+    componentWillMount = () => {
+        Slingshot.fileRestrictions("avatar", {
+            allowedFileTypes: ["image/png", "image/jpeg", "image/gif"],
+            maxSize: 2 * 500 * 500
+        });
+    };
     componentDidMount = () => {
         Meteor.call("riderDetails", Meteor.userId(), (err, data) => {
             if (err) {
@@ -55,6 +63,63 @@ class Profile extends Component {
             }
         );
     };
+    getSignedUrl = (file, callback) => {
+        console.log("here you go");
+        const params = {
+            objectName: file.name,
+            contentType: file.type
+        };
+        Meteor.call("uploadProfilePic", file.name, (err, data) => {
+            if (err) {
+                return notify.show("unable to process", "error");
+            }
+            return callback(null, data);
+        });
+    };
+
+    tryToUpload = () => {
+        this.setState({
+            change_loader: true
+        });
+        var userId = Meteor.user()._id;
+        var metaContext = { avatarId: userId };
+        var uploader = new Slingshot.Upload("avatar", metaContext);
+        uploader.send(
+            document.getElementById("avatar").files[0],
+            function(error, downloadUrl) {
+                // you can use refs if you like
+                if (error) {
+                    this.setState({
+                        change_loader: false
+                    });
+                    // Log service detailed response
+                    console.error(
+                        "Error uploading",
+                        uploader.xhr.response,
+                        error
+                    );
+                    notify.show("Failed to upload profile picture.", "error");
+                } else {
+                    this.setState({
+                        change_loader: false
+                    });
+                    // we use $set because the user can change their avatar so it overwrites the url :)
+                    Meteor.users.update(Meteor.userId(), {
+                        $set: { "profile.avatar": downloadUrl }
+                    });
+                    this.setState({ avatar: downloadUrl });
+                    notify.show("Profile Picture Updated", "success");
+                }
+                // you will need this in the event the user hit the update button because it will remove the avatar url
+            }.bind(this)
+        );
+    };
+    handleAvatarChange = e => {
+        this.setState({
+            avatar: URL.createObjectURL(e.target.files[0]),
+            fileName: e.target.files[0].name
+        });
+    };
     render() {
         return (
             <div
@@ -71,7 +136,44 @@ class Profile extends Component {
 
                 <div className="padding-left padding-bottom padding-right">
                     <div className="list">
-                        <div class="item item-icon-left item-button-right">
+                        <div className="item item-avatar item-button-right">
+                            <img
+                                src={
+                                    this.state.avatar
+                                        ? this.state.avatar
+                                        : "/images/profile.png"
+                                }
+                            />
+                            <label className="custom-file-input">
+                                {this.state.fileName
+                                    ? "  " + this.state.fileName
+                                    : "  Browse"}
+                                <input
+                                    type="file"
+                                    accept="image/*"
+                                    name="avatar"
+                                    id="avatar"
+                                    className="custom-file-input"
+                                    onChange={this.handleAvatarChange}
+                                    hidden={true}
+                                />
+                            </label>
+
+                            <LaddaButton
+                                loading={this.state.change_loader}
+                                data-color="##FFFF00"
+                                data-size={S}
+                                data-style={SLIDE_UP}
+                                data-spinner-size={30}
+                                data-spinner-color="#ddd"
+                                data-spinner-lines={12}
+                                className="button button-positive"
+                                onClick={this.tryToUpload}
+                            >
+                                Change
+                            </LaddaButton>
+                        </div>
+                        <div className="item item-icon-left item-button-right">
                             <i className="icon fa fa-user" aria-hidden="true" />{" "}
                             Name
                             <span className="item-note">
@@ -95,7 +197,7 @@ class Profile extends Component {
                                 <i className="fa fa-edit" aria-hidden="true" />
                             </button>
                         </div>
-                        <div class="item item-icon-left">
+                        <div className="item item-icon-left">
                             <i
                                 className="icon fa fa-phone"
                                 aria-hidden="true"
@@ -106,7 +208,7 @@ class Profile extends Component {
                             </span>
                         </div>
                         {this.isDriver && (
-                            <div class="item item-icon-left">
+                            <div className="item item-icon-left">
                                 <i
                                     className="icon fa fa-user"
                                     aria-hidden="true"
@@ -116,7 +218,7 @@ class Profile extends Component {
                             </div>
                         )}
                         {this.isDriver && (
-                            <div class="item item-icon-left">
+                            <div className="item item-icon-left">
                                 <i
                                     className="icon fa fa-car"
                                     aria-hidden="true"
@@ -128,7 +230,7 @@ class Profile extends Component {
                             </div>
                         )}
                         {this.isDriver && (
-                            <div class="item item-icon-left">
+                            <div className="item item-icon-left">
                                 <i
                                     className="icon fa fa-text-width"
                                     aria-hidden="true"
@@ -141,7 +243,7 @@ class Profile extends Component {
                         )}
 
                         {this.isDriver && (
-                            <div class="item item-icon-left">
+                            <div className="item item-icon-left">
                                 <i
                                     className="icon fa fa-star"
                                     aria-hidden="true"
@@ -153,7 +255,9 @@ class Profile extends Component {
                                         stop={5}
                                         emptySymbol="fa fa-star-o fa-2x empty"
                                         fullSymbol="fa fa-star fa-2x full"
-                                        initialRating={this.state.avgUserRating}
+                                        initialRating={Number(
+                                            this.state.avgUserRating
+                                        )}
                                         readonly
                                         style={{
                                             fontSize: "10px"
@@ -163,7 +267,7 @@ class Profile extends Component {
                             </div>
                         )}
 
-                        <div class="item item-icon-left">
+                        <div className="item item-icon-left">
                             <i className="icon fa fa-star" aria-hidden="true" />{" "}
                             Avg Ratings as User
                             <span className="item-note">
@@ -172,7 +276,7 @@ class Profile extends Component {
                                     stop={5}
                                     emptySymbol="fa fa-star-o fa-2x empty"
                                     fullSymbol="fa fa-star fa-2x full"
-                                    initialRating={this.state.avgRating}
+                                    initialRating={Number(this.state.avgRating)}
                                     readonly
                                     style={{
                                         fontSize: "10px"
@@ -184,7 +288,7 @@ class Profile extends Component {
                     {this.state.isEdit && (
                         <div className="list">
                             <labl>{this.state.placeholder}</labl>
-                            <span class="item item-input">
+                            <span className="item item-input">
                                 <input
                                     type="text"
                                     name={this.state.name_input}
