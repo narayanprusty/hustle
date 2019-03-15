0dd946ef037a219a2f6c800079f68cdbeb1fa711
saikat <saikatchakrabortty2@gmail.com>
2019-03-15 14:59:55 +0530

    push notif send function

--- imports/modules/helpers/server/index.js
+++ imports/modules/helpers/server/index.js
@@ -41,11 +41,33 @@ function getEJSTemplate({ fileName }) {
         );
     });
 }
+const sendPushNotification = (title, text, userId) => {
+    const notId = Math.round(new Date().getTime() / 1000);
+
+    const query = {};
+    if (userId) {
+        query["userId"] = userId;
+    }
+    Push.send({
+        from: "hustle",
+        title,
+        text,
+        sound: "default",
+        query: query,
+        apn: {
+            sound: "default"
+        },
+        contentAvailable: 1,
+        androidChannel: "PushPluginChannel",
+        notId
+    });
+};
 
 export {
     generateRandomString,
     generateCompleteURLForEmailVerification,
     generateCompleteURLForPasswordReset,
     generateCompleteURLForUserInvite,
-    getEJSTemplate
+    getEJSTemplate,
+    sendPushNotification
 };
